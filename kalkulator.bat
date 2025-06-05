e5aabdbb160d28cad240af9c3bc2e3235fd8d6a7
Zinc-zn <riskiafifudin05@gmail.com>
2025-06-05 09:46:13 +0700

    clear

--- kalkulator.bat
+++ kalkulator.bat
@@ -14,16 +14,18 @@ echo [1] Penjumlahan (+)
 echo [2] Pengurangan (-)
 echo [3] Perkalian (*)
 echo [4] Pembagian (/)
+echo [5] Pangkat (^)
 echo [0] Keluar
 echo.
 echo ================================================
 
-set /p pilihan=Masukkan pilihan (0-4): 
+set /p pilihan=Masukkan pilihan (0-5): 
 
 if "%pilihan%"=="1" goto penjumlahan
 if "%pilihan%"=="2" goto pengurangan
 if "%pilihan%"=="3" goto perkalian
 if "%pilihan%"=="4" goto pembagian
+if "%pilihan%"=="5" goto pangkat
 if "%pilihan%"=="0" goto keluar
 goto invalid
 
@@ -96,13 +98,38 @@ echo.
 pause
 goto menu
 
+:pangkat
+cls
+echo ================================================
+echo                 PANGKAT
+echo ================================================
+echo.
+set /p base=Masukkan bilangan dasar: 
+set /p exp=Masukkan pangkat: 
+
+set /a hasil=1
+set /a counter=0
+
+:loop_pangkat
+if %counter% geq %exp% goto selesai_pangkat
+set /a hasil=%hasil% * %base%
+set /a counter=%counter% + 1
+goto loop_pangkat
+
+:selesai_pangkat
+echo.
+echo Hasil: %base%^%exp% = %hasil%
+echo.
+pause
+goto menu
+
 :invalid
 cls
 echo ================================================
 echo              PILIHAN TIDAK VALID
 echo ================================================
 echo.
-echo Silakan pilih angka antara 0-4!
+echo Silakan pilih angka antara 0-5!
 echo.
 pause
 goto menu
